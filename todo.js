58a2726066f0818d316904d778695c3ecb605a19
Shamsudeen <zakariyyahshamsudeen@gmail.com>
2023-11-01 08:39:03 +0100

    local storage updated

--- todo.js
+++ todo.js
@@ -99,10 +99,32 @@ function removeTask(e) {
             e.target.parentElement.remove()
             
             // remove from LS
+
+            removeTaskFromLocalStorage(e.target.parentElement)
          
       }
     }
 }
+
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach(function (task, index) {
+
+        if(taskItem.innerText === task) {
+            console.log('delete idiot')
+            tasks.splice(index, 1)
+        }
+    });
+
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+}
+
 
 function clearTasks() {
     // taskList.innerHTML = ""
